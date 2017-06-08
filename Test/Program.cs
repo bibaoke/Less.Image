83171e7005f225731800fdd072dad35ef2a94dee
Jade <jade@bibaoke.com>
2017-06-08 14:25:21 +0800

    Signed-off-by: Jade <jade@bibaoke.com>

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using Less.Image;
+using Less.Windows;
+using Less.Text;
+using System.Drawing;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Image test1 = Image.FromFile(Application.SetupDir.CombinePath("yangmi.jpg")).Resize(1024, 768);
+
+            test1.Save(Application.SetupDir.CombinePath("yangmi_1024_768.jpg"));
+        }
+    }
+}
