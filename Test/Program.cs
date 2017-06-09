9e436bcb6d2410f1babc037df88a9a87864ebf5a
Jade <jade@bibaoke.com>
2017-06-09 17:19:52 +0800

    Signed-off-by: Jade <jade@bibaoke.com>

--- Test/Program.cs
+++ Test/Program.cs
@@ -14,9 +14,25 @@ namespace Test
 
             Image origin = Image.FromFile(file);
 
-            Image test1 = origin.Resize(600, 400, Color.Black);
+            //test1
+            Image test1 = origin.Resize(600, 400);
 
-            test1.Save(Application.SetupDir.CombinePath("yangmi_600_400.jpg"), 100);
+            test1.Save(Application.SetupDir.CombinePath("yangmi_600_400.jpg"), 90);
+
+            //test2
+            Image test2 = origin.Resize(600, 400, ResizeMode.WidthFirst);
+
+            test2.Save(Application.SetupDir.CombinePath("yangmi_600_400_WidthFirst.jpg"), 90);
+
+            //test3
+            Image test3 = origin.ResizeW(600);
+
+            test3.Save(Application.SetupDir.CombinePath("yangmi_W600.jpg"), 90);
+
+            //test4
+            Image test4 = origin.ResizeW(400);
+
+            test4.Save(Application.SetupDir.CombinePath("yangmi_H400.jpg"), 90);
         }
     }
 }
