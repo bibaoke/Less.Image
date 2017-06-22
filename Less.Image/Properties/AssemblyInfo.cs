08c8d12ad3b6f43dfb74f8b78fb5b29e52663ac1
Jade <jade@bibaoke.com>
2017-06-22 16:20:57 +0800

    Signed-off-by: Jade <jade@bibaoke.com>

--- Less.Image/Properties/AssemblyInfo.cs
+++ Less.Image/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Less.Image")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("一个简单的 C# 图片缩放模块")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Less.Image")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Shao Zhuang")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
